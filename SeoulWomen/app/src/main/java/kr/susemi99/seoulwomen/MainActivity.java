0f53f1343bce539e306cb1f0b0efb65d2d3d5025
Changhoon <susemi99@gmail.com>
2016-03-05 12:20:02 +0900

    add lambda

--- SeoulWomen/app/src/main/java/kr/susemi99/seoulwomen/MainActivity.java
+++ SeoulWomen/app/src/main/java/kr/susemi99/seoulwomen/MainActivity.java
@@ -108,14 +108,6 @@ public class MainActivity extends AppCompatActivity
     {
 
     }
-    else if (id == R.id.nav_share)
-    {
-
-    }
-    else if (id == R.id.nav_send)
-    {
-
-    }
 
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     drawer.closeDrawer(GravityCompat.START);
