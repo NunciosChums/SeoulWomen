f3792b7067009abca40139eb759b3e97f651e140
Changhoon <susemi99@gmail.com>
2016-08-03 18:56:45 +0900

    remove ChromeTab

--- SeoulWomen/app/src/main/java/kr/susemi99/seoulwomen/application/MyApp.java
+++ SeoulWomen/app/src/main/java/kr/susemi99/seoulwomen/application/MyApp.java
@@ -2,6 +2,7 @@ package kr.susemi99.seoulwomen.application;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.v7.app.AppCompatDelegate;
 
 public class MyApp extends Application
 {
@@ -12,6 +13,7 @@ public class MyApp extends Application
   {
     super.onCreate();
     context = getApplicationContext();
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
   }
 
   public static Context context()
