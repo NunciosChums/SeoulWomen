fc9e6174fad45ccca3f6e3607a83bb3440e0f010
진창훈 <susemi99@gmail.com>
2022-10-03 01:02:38 +0900

    안 쓰는 코드 제거

--- SeoulWomen/app/src/main/java/kr/susemi99/seoulwomen/extension/AndroidViewModelExtension.kt
+++
@@ -1,7 +0,0 @@
-package kr.susemi99.seoulwomen.extension
-
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-
-val AndroidViewModel.context
-  get() = getApplication<Application>()
