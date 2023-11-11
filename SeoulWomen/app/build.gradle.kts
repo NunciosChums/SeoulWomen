fd1e6af95825f9225bda9e932b537d50c6d3bd07
USA_USB <nuncios05.chums@icloud.com>
2023-11-11 17:08:32 +0900

    라이브러이 업데이트

--- SeoulWomen/app/build.gradle.kts
+++ SeoulWomen/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
     compose = true
   }
   composeOptions {
-    kotlinCompilerExtensionVersion = "1.5.3" // https://developer.android.com/jetpack/androidx/releases/compose-compiler
+    kotlinCompilerExtensionVersion = "1.5.4" // https://developer.android.com/jetpack/androidx/releases/compose-compiler
   }
   packaging {
     resources {
@@ -50,9 +50,9 @@ android {
 }
 
 dependencies {
-  val lifecycleVersion = "2.6.1"
+  val lifecycleVersion = "2.6.2"
 
-  val composeBom = platform("androidx.compose:compose-bom:2023.08.00") // https://developer.android.com/jetpack/compose/setup#bom-version-mapping
+  val composeBom = platform("androidx.compose:compose-bom:2023.10.01") // https://developer.android.com/jetpack/compose/setup#bom-version-mapping
   implementation(composeBom)
   androidTestImplementation(composeBom)
   debugImplementation(composeBom)
@@ -61,9 +61,9 @@ dependencies {
   implementation("androidx.compose.material3:material3")
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-tooling-preview")
-  implementation("androidx.core:core-ktx:1.10.1")
+  implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
-  implementation("androidx.activity:activity-compose:1.7.2")
+  implementation("androidx.activity:activity-compose:1.8.0")
 
   testImplementation("junit:junit:4.13.2")
   debugImplementation("androidx.compose.ui:ui-tooling")
@@ -87,11 +87,11 @@ dependencies {
   implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
 
   // pagination
-  implementation("androidx.paging:paging-compose:3.2.0")
+  implementation("androidx.paging:paging-compose:3.2.1")
 
   // hilt
-  implementation("com.google.dagger:hilt-android:2.48")
-  kapt("com.google.dagger:hilt-android-compiler:2.48")
+  implementation("com.google.dagger:hilt-android:2.48.1")
+  kapt("com.google.dagger:hilt-android-compiler:2.48.1")
 
   // preference
   implementation("com.chibatching.kotpref:kotpref:2.13.2")
