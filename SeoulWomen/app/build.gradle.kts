19e8e47527d410f47a8a5ccdaa22b04a34cac589
NunciosChums <nuncios05.chums@icloud.com>
2025-07-03 23:23:46 +0900

    라이브러리 업데이트

--- SeoulWomen/app/build.gradle.kts
+++ SeoulWomen/app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
   namespace = "kr.susemi99.seoulwomen"
-  compileSdk = 35
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "kr.susemi99.seoulwomen"
     minSdk = 26
-    targetSdk = 35
+    targetSdk = 36
     versionCode = 12
     versionName = "4"
 
@@ -31,11 +31,11 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
-  kotlinOptions {
-    jvmTarget = "11"
+  kotlin {
+    jvmToolchain(17)
   }
   buildFeatures {
     compose = true
